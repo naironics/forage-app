c0283742fe5c178452a4ac85be5e73a5f216af88
naironics <naironics@gmail.com>
2016-11-20 15:39:56 -0800

    adding code to the Application class to extend SpringBootServletInitializer class, in order to make the app deployable and able to be run in a separate web container like tomcat, apart from running in command line as a standalone application

--- src/main/java/com/naironics/forageapp/ForageApplication.java
+++ src/main/java/com/naironics/forageapp/ForageApplication.java
@@ -2,16 +2,23 @@ package com.naironics.forageapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class ForageApplication {
+public class ForageApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForageApplication.class, args);
 	}
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ForageApplication.class);
+    }
 	
 	
 	@RequestMapping(path = "/")
